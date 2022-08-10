416951f23168bacbb2992368a179b82f9c9ab490
Dan Erdelt <erd@conceptual.ca>
2022-08-10 00:19:44 +0000

    ...

--- install/win/win11.cmd
+++ install/win/win11.cmd
@@ -1,21 +1,25 @@
-wsl --status > nul 2>&1 || goto enableWSL
-
-echo %tmpPath%
+wsl --status > nul 2>&1 || goto installWSL
 
 wsl --update
-
 echo wsl --shutdown
 wsl --shutdown
 wsl --set-default-version 2 > nul 2>&1
 
 echo WSL ready.
 
-
+set "repo=https://raw.githubusercontent.com/cpbuildtools/dev-host/main"
+set "rng=%random%%random%%random%"
+set "tmpPath=%temp%\dev-host-installer-%rng%"
+mkdir %tmpPath% > nul 2>&1
+curl --ssl --silent  %repo%/install/win/win-host.cmd > %tmpPath%\install.cmd
+cd %tmpPath%
+install.cmd
 goto :eof
 
 
-:enableWSL
-echo Enabling Windows Subsystem for Linux. This can take a while...
+
+:installWSL
+echo Installing Windows Subsystem for Linux. This can take a while...
 wsl --install
 
 reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\RunOnce" /v "devHostInstaller" /d "%tmpPath%\install.cmd" /f
